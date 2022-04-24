42cfc35e33e7b74a4c6037f344c0fedb6246a69e
Leonarix58 <leonardominecarlos@gmail.com>
2022-04-23 21:53:18 -0300

    Canvas file, createButton

---
+++ classes/Canvas.js
@@ -0,0 +1,82 @@
+export class canvas {
+
+    constructor(app, Util) {
+        this.app = app
+        this.Util = Util
+    }
+
+    createButton(color, colorHover, texture, hover, width, height, x, y, onClickCode) {
+// Console Warns
+        if (color && texture) {
+            console.log('You can not provide a color and a texture for a Button!')
+            return
+        }
+        if (hover && !texture) {
+            console.log('You can not provide a hover texture, and not provide a button texture!')
+            return
+        }
+
+        if (!width) {
+            console.log('You need to provite Width!')
+            return
+        }
+
+        if (!height) {
+            console.log('You need to provite Height!')
+            return
+        }
+
+// Real code
+        let button
+
+        if (color) {
+            button = new PIXI.Sprite(PIXI.Texture.WHITE)
+            button.tint = color
+        }
+
+        if (texture) {
+            button = PIXI.Sprite.from(texture)
+        }
+
+        if (!x && !y) {
+            button.position.set((this.app.screen.width) / 2, (this.app.screen.height) / 2)
+        } else {
+            button.position.set(x, y)
+        }
+
+        button.width = width
+        button.height = height
+        
+        button.interactive = true
+        button.buttonMode = true 
+
+        this.app.stage.addChild(button);
+
+        button
+            .on('pointerdown', buttonClick)
+            .on('pointerover', buttonOver)
+            .on('pointerout', buttonOut)
+
+        function buttonClick() {
+            onClickCode()
+        }
+        
+        function buttonOver() {
+            if (hover) {
+                this.texture = hover
+            }
+            if (colorHover) {
+                this.tint = colorHover
+            }
+        }
+        
+        function buttonOut() {
+            if (texture) {
+                this.texture = texture
+            }
+            if (color) {
+                this.tint = color
+            }
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { codeBlock } from './classes/CodeBlock.js'
 import { Util } from './classes/Util.js'
+import { canvas } from './classes/Canvas.js'
 
 export const app = new PIXI.Application({
     width: 780,
@@ -14,8 +15,15 @@ document.body.appendChild(app.view)
 let codeBlockClass = new codeBlock(app, Util)
 codeBlockClass.createCodeBlock()
 
+let canvasClass = new canvas(app, Util)
+//canvasClass.createButton({color: '0xFFFFFF',colorHover: '0x000000',width: 100,height: 100, onClickCode: runClick()})
+canvasClass.createButton('0xFFFFFF','0x000000',undefined,undefined,100,100,undefined,undefined,runClick)
+
+function runClick() {
+    console.log('ebaaaa')
+}
+
 document.addEventListener('keydown', (event) => {
-    //notConsole.console()
 })
 
 app.ticker.add(() => {
