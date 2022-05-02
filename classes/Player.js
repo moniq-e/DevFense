cd4d860df1d6ce81f7b86e149896a90e635d6350
Monique <79199057+moniq-e@users.noreply.github.com>
2022-05-02 14:56:10 -0300

    ./

--- classes/Player.js
+++ classes/Player.js
@@ -26,9 +26,9 @@ export class Player {
      */
     createSprite() {
         let playerTextures = [
-            PIXI.Texture.from('../images/player.png'),
+            PIXI.Texture.from('./images/player.png'),
             // textura do red_player ficou com um problema, vou refazer tudo depois
-            PIXI.Texture.from('../images/red_player.png')
+            PIXI.Texture.from('./images/red_player.png')
         ]
 
         let sprite = new PIXI.Sprite.from('images/player.png')
