ce5dcfd1e68f719a94f818f09ffea4becc31a3b4
Leonarix58 <leonardominecarlos@gmail.com>
2022-05-06 22:06:15 -0300

    Blank Level Class

---
+++ classes/Level.js
@@ -0,0 +1,10 @@
+export class Level {
+    currentLevel
+    towerDamage
+    towerMaxHealth
+    zombieDamage
+    zombieMaxHelth
+}
+/*
+Eu tenho uma ideia, mas não sei se precisa ser exatamente uma class, ou uma função com objeto, então já criei o arquivo
+*/
